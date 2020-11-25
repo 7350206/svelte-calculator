5a7e91b8aea90a5bf14d90ff76b20f55bacbbe86
pl <7350206@pl.com>
2020-11-25 23:38:24 +0300

    css described

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,23 @@
 <script>
 	import Title from './Title.svelte';
 	import Navbar from './Navbar.svelte';
-
+  import Example from './Example.svelte';
 </script>
 
 <style>
+  /* works locally  <span>hi from app</span>*/
+  /* span {
+    background: blue;
+    color: white;
+  } */
 </style>
+
+
 
 <Navbar/>
 <Title title="Add expence"/>
 <Title title="Expence list"/>
 <Title />
+
+<Example/>
+<span>hi from app</span>
---
+++ src/Example.svelte
@@ -0,0 +1,29 @@
+<style>
+  /* works locally
+  BUT
+  can be set all [spans] in project be like that
+  set that span as global */
+  :global(span) {
+    /* can be accessed global css vars */
+    background: var(--primaryColor);
+    color: white;
+  }
+
+  /* global styles can be overriden locally
+  with adding custom class to original class
+  main-title svelte-sdfdffsfs  */
+  .main-title{
+    color: red;
+  }
+</style>
+<span>
+  hi from an example
+  <!-- css comes from global
+    will look exactly as needed -->
+      <div class='main-title'>
+        <h3>example title</h3>
+      </div>
+</span>
+
+
+
