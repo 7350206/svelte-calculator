4c6631a36def29dd4a39e654bea54c0fe5241ec5
pl <7350206@pl.com>
2020-11-25 23:06:44 +0300

    props basic. default prop value

---
+++ src/Navbar.svelte
@@ -0,0 +1,9 @@
+<!-- styles comes from global.css -->
+<nav class="nav">
+    <div class='nav-center'>
+      <h1 class='nav-center'>expence calculator</h1>
+      <button type="button" class='nav-btn'>
+        <i class='far fa-plus-square'></i>&nbsp;add
+      </button>
+    </div>
+  </nav>
---
+++ src/Title.svelte
@@ -0,0 +1,8 @@
+<script>
+  // prop here, with default value
+  export let title = "default title"
+</script>
+
+<div class='main-title'>
+  <h3>{title}</h3>
+</div>
