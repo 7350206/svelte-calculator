f4078574649928731c7dc0fd71bb9360433a04f5
pl <7350206@pl.com>
2020-11-27 13:08:56 +0300

    feat: validate empty inputs and disabling submit button with reactive checkin 'isEmpty'

--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -6,8 +6,23 @@
   let amount = null
 
   // $: console.log({name, amount})
+
+// !! truthy value is TRUE when
+// encountered in boolean context
+// "" and null are both falsy
+/* if (name) {
+  console.log(name)
+}*/
+// return true if both of them are false
+$: isEmpty = !name || !amount
+
+
+
+
 </script>
 
+
+{isEmpty}
 <!-- <p>name: {name}</p>
 <p>amount: {amount}</p> -->
 <section class='form'>
@@ -19,13 +34,27 @@
       <input type='text' id="name" bind:value={name}>
     </div>
     <div class='form-control'>
+      <!-- disabled button class example
+      class={isActive ? 'active' :''}
+      class:active={isActive} svelte way
+      class:active={active} if
+      -->
+
+
       <!-- need input with the same id -->
       <label for='amount'>Amount</label>
       <input type='number' id="amount" bind:value={amount}>
     </div>
-    <p class='form-empty'>fill all form fields</p>
-    <button type='submit' class='btn btn-block'>
-      add expense
+    {#if isEmpty}
+      <p class='form-empty'>fill all form fields</p>
+    {/if}
+    <button
+      type='submit'
+      class='btn btn-block'
+      class:disabled={isEmpty}
+      disabled={isEmpty}
+    >
+    add expense
     </button>
     <button type="button" class='close-btn'>
       <i class='fas fa-times'></i>
